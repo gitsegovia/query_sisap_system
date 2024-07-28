594d14ab07df5598fb879e3232b2dcac6c9b04ed
Jose Segovia <jose.segovia.r@gmail.com>
2024-07-28 19:28:02 -0400

    3-1014

--- src/routes.js
+++ src/routes.js
@@ -543,7 +543,7 @@ router.get("/hoja_vida/consulta_dep/:cod_dep", async (req, res) => {
     if (cod_dep == 1014) {
       return IS_ONLY_LN
         ? `f.cod_dep=${cod_dep} and f.cod_tipo_nomina in (1,2,3,8) and hn.clasificacion_personal not in (7,8,13,3,4,6,15,9,10,11,12,13,14)`
-        : `f.cod_dep=${cod_dep} and f.cod_tipo_nomina not in (11,12,13,14) and hn.clasificacion_personal not in (7,8,13,3,4,6,15,9,10,11,12,13,14)`;
+        : `f.cod_dep=${cod_dep} and f.cod_tipo_nomina not in (3,11,12,13,14) and hn.clasificacion_personal not in (7,8,13,3,4,6,15,9,10,11,12,13,14)`;
     }
     if (cod_dep == 1015) {
       return IS_ONLY_LN
