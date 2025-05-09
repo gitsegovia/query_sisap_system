3a7b889d8ea812505e9609e459e4c028755aa0bb
Salvador Ron <salvadorronosio@gmail.com>
2025-05-09 10:41:30 -0400

    fix nomina 6 1006

--- src/routes.js
+++ src/routes.js
@@ -510,7 +510,7 @@ router.get("/hoja_vida/consulta_dep/:cod_dep", async (req, res) => {
     if (cod_dep == 1006) {
       return IS_ONLY_LN
         ? `f.cod_dep=${cod_dep} and f.cod_tipo_nomina in (1,2) and hn.clasificacion_personal not in (7,8,13,3,4,6,15,9,10,11,12,13,14)`
-        : `f.cod_dep=${cod_dep} and f.cod_tipo_nomina not in (7,8,9,10,11,12) and hn.clasificacion_personal not in (7,8,13,3,4,6,15,9,10,11,12,13,14)`;
+        : `f.cod_dep=${cod_dep} and f.cod_tipo_nomina not in (7,8,9,10,11,12) and hn.clasificacion_personal not in (7,8,13,3,4,6,15,9,10,11,13,14)`;
     }
     if (cod_dep == 1007) {
       return IS_ONLY_LN
