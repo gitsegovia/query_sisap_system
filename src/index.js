e2ea47bdd0bc147e0d33746c46950154aca1eb74
Jose Segovia <jose.segovia.r@gmail.com>
2025-07-29 20:23:09 -0400

    add cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 import routes from "./routes";
 import { engine } from "express-handlebars";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,6 +15,7 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(cors("*"));
 //Routers
 app.use(routes);
 
