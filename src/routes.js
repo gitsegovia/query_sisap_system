2f54b8a02204cac1b9b621e510eecb1a97ae3f42
Jose Segovia <jose.segovia.r@gmail.com>
2026-02-24 20:50:35 -0400

    Change tab query fusamie t. to f.

--- src/routes.js
+++ src/routes.js
@@ -39,7 +39,7 @@ router.get("/fusamiebg/consulta/:cedula?", async (req, res) => {
     const CURRENT_YEAR = new Date().getFullYear();
     const sqlQuery = `SELECT cedula_identidad, primer_nombre || ' ' || segundo_nombre || ' ' || primer_apellido || ' ' || segundo_apellido as nombre, f.deno_cod_secretaria, f.deno_cod_direccion, f.demonimacion_puesto, f.cod_dep, f.cod_ficha, f.fecha_nacimiento, f.sexo, f.grupo_sanguineo, f.fecha_ingreso, f.direccion_habitacion, f.telefonos_habitacion, f.carnet, hn.periodo_desde FROM v_cnmd06_fichas_2 as f FULL OUTER JOIN cnmd08_historia_transacciones as t on f.cod_ficha=t.cod_ficha and f.cod_cargo=t.cod_cargo FULL OUTER JOIN cnmd08_historia_nomina as hn on hn.cod_dep=t.cod_dep and hn.cod_tipo_nomina=t.cod_tipo_nomina and hn.numero_nomina=t.numero_nomina and hn.ano=t.ano where f.cedula_identidad=${cedula} and t.ano=${CURRENT_YEAR} and t.cod_transaccion=103 [condition_ext] order by hn.periodo_desde DESC LIMIT 1`;
 
-    const query = await specificQuery({ sqlQuery, table: "t.", db: 1 });
+    const query = await specificQuery({ sqlQuery, table: "f.", db: 1 });
     const checkQuery = query.length; // Object.values(query).reduce((acc, current) => acc + current.length, 0);
 
     if (checkQuery > 0) {
@@ -95,7 +95,7 @@ router.get("/fusamiebg/consulta/:cedula?", async (req, res) => {
       const sqlQuery_obrero = `SELECT cedula_identidad, primer_nombre || ' ' || segundo_nombre || ' ' || primer_apellido || ' ' || segundo_apellido as nombre, 
       f.deno_cod_secretaria, f.deno_cod_direccion, f.demonimacion_puesto, f.cod_dep, f.cod_ficha, f.fecha_nacimiento, f.sexo, f.grupo_sanguineo, 
       f.fecha_ingreso, f.direccion_habitacion, f.telefonos_habitacion, f.carnet, hn.periodo_desde FROM v_cnmd06_fichas_2 as f FULL OUTER JOIN cnmd05 as t on f.cod_ficha=t.cod_ficha and f.cod_cargo=t.cod_cargo FULL OUTER JOIN cnmd01 as hn on hn.cod_dep=t.cod_dep and hn.cod_tipo_nomina=t.cod_tipo_nomina where f.cedula_identidad=${cedula} and t.ano=${CURRENT_YEAR} and f.condicion_actividad_ficha=1 and hn.clasificacion_personal in (2,8,10) [condition_ext] order by hn.periodo_desde DESC LIMIT 1`;
-      const query_obrero = await specificQuery({ sqlQuery: sqlQuery_obrero, table: "t.", db: 1 });
+      const query_obrero = await specificQuery({ sqlQuery: sqlQuery_obrero, table: "f.", db: 1 });
 
       const checkQuery = query_obrero.length; //Object.values(query_obrero).reduce((acc, current) => acc + current.length, 0);
 
@@ -788,7 +788,7 @@ router.get("/hoja_vida/lista_empleados/", async (req, res) => {
     condition = IS_ONLY_LN
       ? condition.concat(`( f.cod_dep=1 and f.cod_secretaria=1 and f.cod_direccion NOT IN (2,3,4,5) and f.cod_tipo_nomina in (1,2,3) ) `)
       : condition.concat(
-          `( f.cod_dep=1 and f.cod_secretaria=1 and f.cod_direccion NOT IN (2,3,4,5) and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `
+          `( f.cod_dep=1 and f.cod_secretaria=1 and f.cod_direccion NOT IN (2,3,4,5) and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `,
         );
     //condition = ;
 
@@ -797,7 +797,7 @@ router.get("/hoja_vida/lista_empleados/", async (req, res) => {
       condition = IS_ONLY_LN
         ? condition.concat(`OR ( f.cod_dep=1 and f.cod_secretaria=${codSplit[0]} and f.cod_direccion=${codSplit[1]} and f.cod_tipo_nomina in (1,2,3) ) `)
         : condition.concat(
-            `OR ( f.cod_dep=1 and f.cod_secretaria=${codSplit[0]} and f.cod_direccion=${codSplit[1]} and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `
+            `OR ( f.cod_dep=1 and f.cod_secretaria=${codSplit[0]} and f.cod_direccion=${codSplit[1]} and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `,
           );
       //condition = ;
     });
@@ -805,28 +805,28 @@ router.get("/hoja_vida/lista_empleados/", async (req, res) => {
     condition = IS_ONLY_LN
       ? condition.concat(`OR ( f.cod_dep=1 and f.cod_secretaria=10 and f.cod_direccion NOT IN (8,9) and f.cod_tipo_nomina in (1,2,3) ) `)
       : condition.concat(
-          `OR ( f.cod_dep=1 and f.cod_secretaria=10 and f.cod_direccion NOT IN (8,9) and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `
+          `OR ( f.cod_dep=1 and f.cod_secretaria=10 and f.cod_direccion NOT IN (8,9) and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `,
         );
     //condition = ;
 
     condition = IS_ONLY_LN
       ? condition.concat(`OR ( f.cod_dep=1 and f.cod_secretaria=13 and f.cod_direccion NOT IN (2,3,4,5,8) and f.cod_tipo_nomina in (1,2,3) ) `)
       : condition.concat(
-          `OR ( f.cod_dep=1 and f.cod_secretaria=13 and f.cod_direccion NOT IN (2,3,4,5,8) and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `
+          `OR ( f.cod_dep=1 and f.cod_secretaria=13 and f.cod_direccion NOT IN (2,3,4,5,8) and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `,
         );
     //condition = ;
 
     condition = IS_ONLY_LN
       ? condition.concat(`OR ( f.cod_dep=1 and f.cod_secretaria=15 and f.cod_direccion NOT IN (1,2,3,4,5,8,9) and f.cod_tipo_nomina in (1,2,3) ) `)
       : condition.concat(
-          `OR ( f.cod_dep=1 and f.cod_secretaria=15 and f.cod_direccion NOT IN (1,2,3,4,5,8,9) and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `
+          `OR ( f.cod_dep=1 and f.cod_secretaria=15 and f.cod_direccion NOT IN (1,2,3,4,5,8,9) and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `,
         );
     //condition = ;
 
     condition = IS_ONLY_LN
       ? condition.concat(`OR ( f.cod_dep=1 and f.cod_secretaria in (02,03,05,06,07,08,09,11,12,14,16,17,18,19,20,21) and f.cod_tipo_nomina in (1,2,3,29) ) `)
       : condition.concat(
-          `OR ( f.cod_dep=1 and f.cod_secretaria in (02,03,05,06,07,08,09,11,12,14,16,17,18,19,20,21) and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `
+          `OR ( f.cod_dep=1 and f.cod_secretaria in (02,03,05,06,07,08,09,11,12,14,16,17,18,19,20,21) and f.cod_tipo_nomina not in (10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,33,37,38,39,40,9,6) ) `,
         );
     //condition = ;
 
@@ -847,10 +847,10 @@ router.get("/hoja_vida/lista_empleados/", async (req, res) => {
 
     condition = IS_ONLY_LN
       ? condition.concat(
-          `OR ( f.cod_dep in (1000,1001,1002,1003,1004,1005,1006,1007,1008,1010,1011,1012,1013,1016,1017,1018,1019,1020,1021,1022,1023,1027,1028,1029,1030,1031,1032,1033,1034,1035,1036,1037,1038,1041,1042,1043,1044,1045,1046) and f.cod_tipo_nomina in (1,2) ) `
+          `OR ( f.cod_dep in (1000,1001,1002,1003,1004,1005,1006,1007,1008,1010,1011,1012,1013,1016,1017,1018,1019,1020,1021,1022,1023,1027,1028,1029,1030,1031,1032,1033,1034,1035,1036,1037,1038,1041,1042,1043,1044,1045,1046) and f.cod_tipo_nomina in (1,2) ) `,
         )
       : condition.concat(
-          `OR ( f.cod_dep in (1000,1001,1002,1003,1004,1005,1006,1007,1008,1010,1011,1012,1013,1016,1017,1018,1019,1022,1023,1027,1028,1029,1030,1031,1032,1033,1034,1035,1036,1037,1038,1041,1042,1043,1044,1045,1046) ) `
+          `OR ( f.cod_dep in (1000,1001,1002,1003,1004,1005,1006,1007,1008,1010,1011,1012,1013,1016,1017,1018,1019,1022,1023,1027,1028,1029,1030,1031,1032,1033,1034,1035,1036,1037,1038,1041,1042,1043,1044,1045,1046) ) `,
         );
     /*condition = 
     );*/
@@ -1389,6 +1389,120 @@ router.get("/sisap/temp", async (req, res) => {
   res.json({
     query,
   });
+});
+
+router.get("/sisap/consulta_dep/:cod_dep", async (req, res) => {
+  const { cod_dep } = req.params;
+
+  if (!cod_dep) {
+    res.status(404).send("Dependencia requerida");
+    return false;
+  }
+
+  const getCondition = () => {
+    return `f.cod_dep=${cod_dep} and hn.clasificacion_personal in (1,2,3,4,5,16,12,17,18)`;
+  };
+
+  const getConditionDB = () => {
+    if (cod_dep.includes("-")) {
+      return 1;
+    }
+    if (cod_dep < 1000) {
+      return 1;
+    }
+    if (
+      cod_dep == 1006 ||
+      cod_dep == 1021 ||
+      cod_dep == 1027 ||
+      cod_dep == 1028 ||
+      cod_dep == 1036 ||
+      cod_dep == 1037 ||
+      cod_dep == 1038 ||
+      cod_dep == 1039 ||
+      cod_dep == 1040 ||
+      cod_dep == 1042 ||
+      cod_dep == 1043 ||
+      cod_dep == 1045 ||
+      cod_dep == 1046
+    ) {
+      return 2;
+    }
+    if (cod_dep == 1035) {
+      return 3;
+    }
+    if (cod_dep == 1041) {
+      return 4;
+    }
+    return 1;
+  };
+
+  const condition_dep = getCondition();
+  if (condition_dep === null) {
+    res.status(404).send("Dependencia requerida");
+    return false;
+  }
+
+  if (condition_dep === "") {
+    res.status(404).send({ message: "No existe resultados", error: "" });
+    return false;
+  }
+
+  try {
+    let db = getConditionDB();
+    let beneficiario = [];
+    const CURRENT_YEAR = new Date().getFullYear();
+    const sqlQuery = `SELECT f.cedula_identidad, f.primer_nombre || ' ' || f.segundo_nombre || ' ' || f.primer_apellido || ' ' || f.segundo_apellido as nombre, 
+    f.deno_cod_secretaria, f.cod_secretaria, f.deno_cod_direccion, f.cod_direccion, f.deno_cod_division, f.cod_division, f.deno_cod_departamento, f.cod_departamento, f.demonimacion_puesto, f.cod_dep, f.denominacion_dependencia, f.cod_ficha, f.fecha_nacimiento, f.sexo, CASE 
+            WHEN f.estado_civil='S' THEN 'Soltero' 
+            WHEN f.estado_civil='C' THEN 'Casado' 
+            WHEN f.estado_civil='D' THEN 'Divorciado' 
+            ELSE 'Otro'
+          END estado_civil, f.grupo_sanguineo, 
+    f.correo_electronico,
+    f.deno_cod_estado, f.deno_cod_municipio, f.deno_cod_parroquia, f.deno_cod_centro, f.deno_ciudad,    
+    (select denominacion from cugd01_estados where cod_republica=f.cod_pais_origen and cod_estado=f.cod_estado_origen) as deno_estado_nacimiento,    
+    (select denominacion from cugd01_municipios where cod_republica=f.cod_pais_origen and cod_estado=f.cod_estado_origen and cod_municipio=f.cod_municipio_origen) as deno_municipio_nacimiento,    
+    (select denominacion from cugd01_parroquias where cod_republica=f.cod_pais_origen and cod_estado=f.cod_estado_origen and cod_municipio=f.cod_municipio_origen and cod_parroquia=f.cod_parroquia_origen) as deno_parroquia_nacimiento,
+    (select denominacion from cugd01_centros_poblados where cod_republica=f.cod_pais_origen and cod_estado=f.cod_estado_origen and cod_municipio=f.cod_municipio_origen and cod_parroquia=f.cod_parroquia_origen and cod_centro=f.cod_centropoblado_origen) as deno_centropoblado_nacimiento,
+    (select denominacion from cugd01_estados where cod_republica=1 and cod_estado=f.cod_estado_habitacion) as deno_estado_habitacion,    
+    (select denominacion from cugd01_municipios where cod_republica=1 and cod_estado=f.cod_estado_habitacion and cod_municipio=f.cod_municipio_habitacion) as deno_municipio_habitacion,    
+    (select denominacion from cugd01_parroquias where cod_republica=1 and cod_estado=f.cod_estado_habitacion and cod_municipio=f.cod_municipio_habitacion and cod_parroquia=f.cod_parroquia_habitacion) as deno_parroquia_habitacion,    
+    (select denominacion from cugd01_centros_poblados where cod_republica=1 and cod_estado=f.cod_estado_habitacion and cod_municipio=f.cod_municipio_habitacion and cod_parroquia=f.cod_parroquia_habitacion and cod_centro=f.cod_centropoblado_habitacion) as deno_contropoblado_habitacion,
+    f.cod_centropoblado_habitacion,
+    (select denominacion from cnmd06_profesiones where cod_profesion=dp.cod_profesion) as profesion,
+    (select denominacion from cnmd06_especialidades where cod_profesion=dp.cod_profesion and cod_especialidad=dp.cod_especialidad) as especialidad,
+    f.fecha_ingreso, f.direccion_habitacion, f.telefonos_habitacion, f.carnet,
+    (select devolver_grado_puesto(
+      (select xy.clasificacion_personal from cnmd01 xy where xy.cod_dep=t.cod_dep and xy.cod_tipo_nomina=t.cod_tipo_nomina), t.cod_puesto) )as cod_grado_puesto,
+      hn.cod_tipo_nomina, hn.denominacion, hn.denominacion_devengado
+    FROM v_cnmd06_fichas_2 as f 
+    FULL OUTER JOIN cnmd05 as t on f.cod_dep=t.cod_dep and f.cod_ficha=t.cod_ficha and f.cod_cargo=t.cod_cargo and t.cod_tipo_nomina=f.cod_tipo_nomina
+    FULL OUTER JOIN cnmd01 as hn on hn.cod_dep=t.cod_dep and hn.cod_tipo_nomina=t.cod_tipo_nomina 
+    FULL OUTER JOIN cnmd06_datos_personales as dp on dp.cedula_identidad=f.cedula_identidad 
+    where  ${condition_dep} and t.ano=${CURRENT_YEAR} and f.condicion_actividad_ficha=1 and hn.denominacion_devengado<>'PRESTACIONES' [condition_ext] ORDER BY t.cod_tipo_nomina`;
+
+    const query = await specificQuery({ sqlQuery, table: "f.", db: db });
+    if (query.length > 0) {
+      query.map((emp) => {
+        beneficiario.push({
+          ...emp,
+          edad: diffYear(emp.fecha_nacimiento),
+          antiguedad: diffYear(emp.fecha_ingreso),
+        });
+      });
+    }
+
+    if (beneficiario.length > 0) {
+      const result_employee = beneficiario; //query;
+      res.json(result_employee);
+      return true;
+    } else {
+      res.status(404).send({ message: "No existe resultados", error: "" });
+      return false;
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Error en la consulta unificada", error: error.message });
+  }
 });
 
 export default router;
